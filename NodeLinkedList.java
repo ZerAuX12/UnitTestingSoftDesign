6aeac80e34870c2b3a5e3c10f350f092531a2930
CHRISTIAN ANDREW A. SUAREZ <1320975@cc.usls.edu>
2023-05-18 12:14:32 +0800

    Unit testing Software Design

---
+++ NodeLinkedList.java
@@ -0,0 +1,83 @@
+public class NodeLinkedList {
+    private Node head;
+    private int size=0,number;
+
+
+    public void addToFront(int value){
+        Node node = new Node(value);
+        node.setNext(head);
+        head = node;
+        size++;
+    }
+    public void addToTail(int value){
+
+        Node node = new Node(value);
+        Node node1 = head;
+        if(size == 0){
+            head=node;
+        }
+        else{
+            for(int i = 0; i<size-1; i++){
+                node1 = node1.getNext();
+            }
+            node1.setNext(node);
+        }
+        size++;
+    }
+    public Node removeFromFront(){
+        if (head!=null) {
+            Node newHead = head.getNext();
+            Node oldHead = head;
+            oldHead.setNext(null);
+            head = newHead;
+            size--;
+            return oldHead;
+        }
+        else{
+            return null;
+        }
+
+
+    }
+    public void remove(int value){
+        Node node = head;
+        if(node.getValue()==value){
+            removeFromFront();
+        }
+        else{
+            while(node != null){
+                if(node.getNext()!= null && node.getNext().getValue()==value){
+                    Node nextNode = node.getNext();
+                    node.setNext(node.getNext().getNext());
+                    nextNode.setNext(null);
+                }
+                node=node.getNext();
+            }
+        }
+    }
+    public Node removeTail(){
+        Node x = head;
+        int v=0;
+        if(head != null) {
+            while (x != null) {
+                if (x.getNext().getNext() == null) {
+                    v = x.getNext().getValue();
+                    remove(x.getNext().getValue());
+                }
+                x = x.getNext();
+            }
+            size--;
+        }
+        return null;
+
+    }
+    public int getSize(){
+        return size;
+    }
+    public Node getHead(){
+        return head;
+    }
+    public int getNumber(){
+        return number;
+    }
+}
---
+++ NodeLinkedListTest.java
@@ -0,0 +1,72 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeLinkedListTest {
+    @Test
+    public void shouldaddToFront(){
+        NodeLinkedList list = new NodeLinkedList();
+        assertEquals(0,list.getSize());
+        list.addToFront(5);
+        assertEquals(1,list.getSize());;
+        list.addToFront(10);
+        assertEquals(2, list.getSize());
+        list.addToFront(15);
+        assertEquals(3, list.getSize());
+
+
+    }
+    @Test
+    public void shouldaddToTail(){
+        NodeLinkedList listA = new NodeLinkedList();
+        listA.addToTail(5);
+        assertEquals(1,listA.getSize());
+        listA.addToTail(10);
+        assertEquals(2,listA.getSize());
+    }
+
+    @Test
+    public void shouldremoveFromFront(){
+        NodeLinkedList list = new NodeLinkedList();
+        assertEquals(0,list.getSize());
+        list.addToFront(5);
+        assertEquals(1,list.getSize());;
+        list.addToFront(10);
+        assertEquals(2, list.getSize());
+        list.addToFront(15);
+        assertEquals(3, list.getSize());
+        list.removeFromFront();
+        assertEquals(2, list.getSize());
+
+    }
+    @Test
+    public void shouldremoveFromFrontempty(){
+        NodeLinkedList list = new NodeLinkedList();
+        list.removeFromFront();
+        assertEquals(0,list.getSize());
+        assertEquals(null,list.getHead());
+
+    }
+    @Test
+    public void shouldremoveTail(){
+        NodeLinkedList list = new NodeLinkedList();
+        assertEquals(0,list.getSize());
+        list.addToFront(5);
+        assertEquals(1,list.getSize());;
+        list.addToFront(10);
+        assertEquals(2, list.getSize());
+        list.addToFront(15);
+        assertEquals(3, list.getSize());
+        list.removeTail();
+        assertEquals(2, list.getSize());
+    }
+    @Test
+    public void shouldremoveTailempty(){
+        NodeLinkedList list = new NodeLinkedList();
+        list.removeTail();
+        assertEquals(0, list.getSize());
+        assertEquals(null, list.getHead());
+    }
+
+
+}
